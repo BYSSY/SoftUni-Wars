e4a5ed07239dedc84dd1e77d2710c13f86dd6d9f
Bozhidar Gevechanov <stil.2910@gmail.com>
2015-12-04 20:05:30 +0200

    first commit

---
+++ SoftUniWarz/SoftUniWarz/Characters/Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SoftUniWarz.Interfaces;
+
+namespace SoftUniWarz
+{
+    class Player : Players,IAttackable
+    {
+    }
+}
