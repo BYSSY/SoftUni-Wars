6f92fab07781f82e79b6cf215c2f41c99550b19f
Bozhidar Gevechanov <stil.2910@gmail.com>
2015-12-16 13:13:31 +0200

    Changing sprites

--- SoftUniWarz/SoftUniWarz/Attack/ConstructorLegoAttack.cs
+++ SoftUniWarz/SoftUniWarz/Attack/ConstructorLegoAttack.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 
 namespace SoftUniWarz.Attack
 {
-    class SimpleEnemyAttack : Attack
+    public class ConstructorLegoAttack : Attack
     {
+        //TODO: FIX stats of attack
         private static readonly int damageTake = 70;
         private static readonly int manaTake = 5;
         private static readonly int coffeeTake = 0;
         private static readonly int beerTake = 0;
         private static readonly bool isParalizable = false;
         private static readonly string texturePath = "";
-        //TODO : take random image from 3 
         //TODO: fix sizes of images
         private static readonly int width;
         private static readonly int height;
 
-        public SimpleEnemyAttack(Vector2 position)
+        public ConstructorLegoAttack(Vector2 position)
             : base(damageTake, manaTake, coffeeTake, beerTake, isParalizable, texturePath, position, width, height)
         {
         }
