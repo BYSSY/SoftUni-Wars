de67e92b5a4526c3d5a9b41a14e2bf5a149b95c3
Gesh4o <georgi.stoimenov_96@abv.bg>
2015-12-16 13:05:02 +0200

    Added Validation folder and updated Character class

---
+++ SoftUniWarz/SoftUniWarz/Validation/ValidateData.cs
@@ -0,0 +1,27 @@
+﻿namespace SoftUniWarz.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class ValidateData
+    {
+        // Validating info from the proeperties.
+        public static void ValidateNumber(int bottomBoundary, int upperBoundary, int numberToValidate, string paramName)
+        {
+            if (numberToValidate < bottomBoundary || numberToValidate > upperBoundary)
+            {
+                throw new ArgumentOutOfRangeException(string.Format($"{paramName} must be in range[{bottomBoundary},{upperBoundary}]"));
+            }
+        }
+
+        public static void CheckIsNull (object objectToValidate, string name)
+        {
+            if (objectToValidate == null)
+            {
+                throw new ArgumentNullException(string.Format($"{name} cannot be null!"));
+            }
+        }
+    }
+}
