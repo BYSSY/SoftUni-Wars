fd1c2b7cf8010ddfbfd7bab5a40712aa007436a4
Gesh4o <georgi.stoimenov_96@abv.bg>
2015-12-16 14:33:44 +0200

    Enemy class update

--- SoftUniWarz/SoftUniWarz/Characters/Enemy/Enemy.cs
+++ SoftUniWarz/SoftUniWarz/Characters/Enemy/Enemy.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SoftUniWarz.Characters.Enemy
+﻿namespace SoftUniWarz.Characters.Enemy
 {
-    public class Enemy
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Xna.Framework;
+
+    public abstract class Enemy : Character
     {
+        private static readonly Vector2 DefaultEnemyPosition = new Vector2(500, 500);
+        private const int DefaultEnemyWidth = 500;
+        private const int DefaultEnemyHeigth = 500;
+
+        public Enemy(
+        string name,
+        int healthPoints,
+        int manaPoints,
+        string texturePath)
+
+        : base(
+            name,
+            healthPoints,
+            manaPoints,
+            texturePath,
+            DefaultEnemyPosition,
+            DefaultEnemyWidth,
+            DefaultEnemyHeigth)
+        {
+
+        }
     }
 }
